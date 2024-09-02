0aeff89d5347049c6923e88edd26a486772e6acc
EurusTrick <ilianjesus14@outlook.com>
2024-09-02 17:14:40 -0600

    add education branch #3

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './work-experience.component.html',
-  styleUrl: './work-experience.component.css'
+  styleUrl: './work-experience.component.css' 
 })
 
 export class WorkExperienceComponent {
