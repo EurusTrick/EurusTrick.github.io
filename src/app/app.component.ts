97b12ff280691712ef8b3db7960865a64bd8a3e6
Ilian Jesus Orduña Herrera <159165682+EurusTrick@users.noreply.github.com>
2024-09-25 17:06:47 -0600

    add interests branch #6 (#11)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
-import { EducationComponent } from "./education/education.component";
+import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
 import { LanguagesComponent } from './languages/languages.component';
+import { InterestsComponent } from './interests/interests.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent, EducationComponent, SkillsComponent, LanguagesComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    WorkExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    LanguagesComponent,
+    InterestsComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mycv';
